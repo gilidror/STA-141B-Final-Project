b40bb24bb3f94de36c22f9327ae66c5d80756284
gilidror <59627384+gilidror@users.noreply.github.com>
2020-03-07 13:29:23 -0800

    Add files via upload

---
+++ sta141bfinalproject.R
@@ -0,0 +1,18 @@
+#Shiny Project
+
+library(shiny)
+library(httr)
+library(jsonlite)
+
+ui <- fluidPage(
+  
+)
+
+server <- function(input, output) {
+  
+}
+
+
+#create shiny app
+shinyApp(ui = ui, server = server)
+
